90e645ecf671711af553eb44f41ee8fd1aff252f
Keisuke Kamioka <keisukekamioka@Keisukenonotobukkukonpyuta.local>
2024-04-21 18:43:03 +0900

    station6

--- src/main/java/com/example/station6/Main.java
+++ src/main/java/com/example/station6/Main.java
@@ -2,7 +2,15 @@ package com.example.station6;
 
 public class Main {
     public static void main(String[] args) {
-        // ここから
-        // ここまで
+        int users[] = new int[5];
+        users[0] = 23;
+        users[1] = 36;
+        users[2] = 31;
+        users[3] = 48;
+        users[4] = 58;
+
+        int all = users[0] + users[1] + users[2] + users[3] + users[4];
+        
+        System.out.println((all + 0.0) / users.length);
     }
 }
